51a11b25228965f83d811a6cd01326ff9ebc087c
Edward Zhou <edward.zhq@gmail.com>
2014-09-03 20:57:37 +0800

    merge local

---
+++ game-server/fix_user_profile.js
@@ -0,0 +1,42 @@
+/**
+ * Created by edwardzhou on 14-8-26.
+ */
+
+
+
+cb = function(err, obj) {
+  console.log('err => ', err);
+  console.log('obj => ', obj);
+  lastErr = err;
+  lastObj = obj;
+};
+
+mongoose = require('mongoose');
+mongoose.connect('mongodb://dev/new_ddz_dev');
+console.log('after connected');
+
+UserId = require('./app/domain/userId');
+User = require('./app/domain/user');
+DdzProfile = require('./app/domain/ddzProfile');
+
+mongoose.connections[0].on('error', cb);
+
+checkDdzProfile = function(user) {
+  DdzProfile.findOneQ({userId: user.userId})
+    .then(function(ddzProfile) {
+      if (ddzProfile == null) {
+        console.log('create ddz profile for userId ', user.userId);
+        ddzProfile = new DdzProfile({userId: user.userId});
+        return ddzProfile.saveQ();
+      }
+    })
+    .done();
+};
+
+fix_profile = function() {
+  User.findQ({})
+    .then(function(users) {
+      users.forEach(checkDdzProfile);
+    });
+};
+
