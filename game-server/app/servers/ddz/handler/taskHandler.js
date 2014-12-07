bb31cadc21a2ae3bc12015ced6718df5fab77cdc
Edward Zhou <edwardzhou@ZhouHuideMacBook-Pro.local>
2014-12-07 22:16:41 +0800

    添加任务系统

---
+++ game-server/app/servers/ddz/handler/taskHandler.js
@@ -0,0 +1,52 @@
+/**
+ * Created by edwardzhou on 14/12/6.
+ */
+
+var format = require('util').format;
+var logger = require('pomelo-logger').getLogger(__filename);
+var utils = require('../../../util/utils');
+var Result = require('../../../domain/result');
+var DdzGoodsPackage = require('../../../domain/ddzGoodsPackage');
+
+
+module.exports = function(app) {
+    return new Handler(app);
+};
+
+var Handler = function(app) {
+    logger.info("connector.TaskHandler created.");
+    this.app = app;
+};
+
+
+/**
+ * 获取任务清单
+ * @param msg
+ * @param session
+ * @param next
+ */
+Handler.prototype.getTasks = function(msg, session, next) {
+
+};
+
+
+/**
+ * 领取任务
+ * @param msg
+ * @param session
+ * @param next
+ */
+Handler.prototype.takeTask = function(msg, session, next) {
+
+};
+
+
+/**
+ * 领取任务奖励
+ * @param msg
+ * @param session
+ * @param next
+ */
+Handler.prototype.takeTaskBonus = function(msg, session, next) {
+
+};
